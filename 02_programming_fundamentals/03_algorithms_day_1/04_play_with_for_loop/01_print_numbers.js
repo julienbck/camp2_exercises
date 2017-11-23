a3b1f8519cdc9bca33d1520dc84d09620299daad
JULIEN BOCK <julien.bock@decathlon.com>
2017-11-24 00:26:31 +0100

    Well done exercices

--- 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/01_print_numbers.js
+++ 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/01_print_numbers.js
@@ -1,1 +1,2 @@
 // Write on `stdout` each number from 0 to 100.
+const stdout = [0, 1, 2, 3, 4, 5, 6, 7, 100];
--- 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/03_iteration.js
+++ 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/03_iteration.js
@@ -2,3 +2,5 @@
 //
 // -  Create an array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
 // -  Using `length`, write on `stdout` each values of the table.
+
+const litteralDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
