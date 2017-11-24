f48a80657aba486b312ad33dc59280bca29f6f97
JULIEN BOCK <julien.bock@decathlon.com>
2017-11-24 14:37:37 +0100

    edit exercices dones

--- 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
+++ 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
@@ -1,12 +1,14 @@
 // Write a function that takes two input parameters, and returns a new array with defaults values between the start
 // number and the end number.
-//
+
+// => [1, 2, 3, 4]
 // This function should work in both ascending or descending order.
 
 // Complete this function.
-function range() {
+function range(firstNumber, lastNumber) {for (let i = 0; i < ascArray.join; i++) {console.log(ascArray[i]);}
 
 }
+const ascArray = range(1, 4);
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
