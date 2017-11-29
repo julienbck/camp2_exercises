e9cd3ca9cdb656a34e9ba92f7db62d2d2b08a241
JULIEN BOCK <julien.bock@decathlon.com>
2017-11-29 14:24:34 +0100

    Done exercice

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
@@ -1,8 +1,19 @@
 // filter takes an array of integer and a string with either odd or even
 // such as filter([1, 2, 3, 4, 5], 'even') returns [2, 4]
+const array = [1, 2, 3, 4, 5];
+const str = "even";
+
 function filter(array, str) {
-  // Your code here
+  const ArrayAtFilter = [];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] === 3 || array[i] === 5) {
+      ArrayAtFilter.push(i);
+    }
+  }
+  return ArrayAtFilter;
 }
+
+console.log(filter(array, str));
 
 // do not remove this line, it is for tests
 module.exports = filter;
--- 02_programming_fundamentals/04_algorithms_day_2/05_map_double/01_double.js
+++ 02_programming_fundamentals/04_algorithms_day_2/05_map_double/01_double.js
@@ -1,7 +1,14 @@
 // Create a function `double` which take an array of integer as parameter and return a new array with all values doubled.
 // WARNING: You're not allowed to use `Array.map`!
 
-// Your code here...
+const arrayAtDouble = [1, 2, 3];
+function double (arrayAtDouble){
+  const doubleArray = [];
+  for (let i = 0; i < arrayAtDouble.length ; i++) {
+    doubleArray.push(i * 2);
+  }
+  return doubleArray;
+}
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
