b320deb8bec753a72261c5018fd7d02909eac6e0
JULIEN BOCK <julien.bock@decathlon.com>
2017-12-05 11:37:31 +0100

    Release exercices curl

--- 03_backend_developement/01_HTTP/04_manage_a_webservice/01_fetch_posts.sh
+++ 03_backend_developement/01_HTTP/04_manage_a_webservice/01_fetch_posts.sh
@@ -1,1 +1,2 @@
 curl -X GET 'http://jsonplaceholder.typicode.com/posts'
+#the same that :  curl 'http://jsonplaceholder.typicode.com/posts'
--- 03_backend_developement/01_HTTP/04_manage_a_webservice/08_publish_post.sh
+++ 03_backend_developement/01_HTTP/04_manage_a_webservice/08_publish_post.sh
@@ -1,1 +1,5 @@
-curl -d "body=body&title=title&userId=1" -X POST 'http://jsonplaceholder.typicode.com/posts'
+echo $1
+echo $2
+echo $3
+curl -d "body=\"$1\"&title=\"$2\"&userId=\"$3\"" -X POST 'http://jsonplaceholder.typicode.com/posts'
+t
