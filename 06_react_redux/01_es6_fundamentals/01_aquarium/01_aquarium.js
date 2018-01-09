dae345706ddddbd90fc79f718090e3fe17fcc896
Julien BOCK <julien.bock@decathlon.com>
2018-01-09 14:42:00 +0100

    update exercices

--- 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
+++ 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
@@ -1,4 +1,34 @@
-// Insert code here
+class Fish {
+  constructor(name, sex, number ) {
+    this.name = name;
+    this.sex = sex;
+    this.number = number;
+  }
+}
+
+class Aquarium {
+  constructor(newFish, newAlgae, newCarnFish, newVeganFish) {
+    this.fishes = newFish;
+    this.aquarium = [newCarnFish, newVeganFish];
+    this.algaes = newAlgae;
+  }
+  addFish (fish) {
+    this.fishes.push(fish);
+  }
+  addAlgae (algae) {
+    this.algaes.push(algae);
+  }
+  endTurn(){
+
+}
+
+class Algae {
+  constructor(number) {
+    this.number = number;
+  }
+}
+
+
 
 
 
